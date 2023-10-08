43acebd997a4d798284a685af3104bdeda4e0279
Venkata Mutyala <venkata@venkatamutyala.com>
2023-10-08 12:15:28 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,10 @@ setup(
     version='0.0.1',
     packages=find_packages(),
     install_requires=[
-        # List your dependencies here
+        requests==2.31.0,
+        boto3==1.28.62,
+        slowapi=0.1.8,
+        cryptography==41.0.4
     ],
     entry_points={
         # If needed, you can add entry points for command line utilities here
