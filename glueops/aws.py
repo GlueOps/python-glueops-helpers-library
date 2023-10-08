1c663476a8ccadb037fe39da7f1b76895fc2c039
Venkata Mutyala <venkata@venkatamutyala.com>
2023-10-08 02:35:21 +0000

    feat: adding aws.py

---
+++ glueops/aws.py
@@ -0,0 +1,28 @@
+import boto3
+import glueops.logging
+
+logger = glueops.logging.configure()
+
+
+def create_aws_client(service):
+    return boto3.client(service, region_name='us-east-1')
+
+
+def get_resource_arns_using_tags(tags, aws_resource_filters):
+    """Retrieve ARNs of resources with specific tags."""
+    logger.info(
+        f"Checking to see if this arn was already requested/created with tags: {tags}")
+    tagging = create_aws_client('resourcegroupstaggingapi')
+    tags = {item['Key']: item['Value'] for item in tags}
+    response = tagging.get_resources(
+        TagFilters=[
+            {'Key': key, 'Values': [value]} for key, value in tags.items()
+        ],
+        # ['cloudfront:distribution'] or ['acm:certificate']
+        ResourceTypeFilters=aws_resource_filters
+    )
+
+    arns = [item['ResourceARN']
+            for item in response.get('ResourceTagMappingList', [])]
+
+    return arns
