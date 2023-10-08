3d0b55b2b4a5487b43104ff6bd8fa214a10a0019
Venkata Mutyala <venkata@venkatamutyala.com>
2023-10-08 13:07:31 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,19 +9,7 @@ setup(
         'requests==2.31.0',
         'boto3==1.28.62',
         'slowapi==0.1.8',
-        'cryptography==41.0.4',
-        'jwt==1.3.1',
-        'kubernetes==28.1.0',
-        'requests==2.31.0',
-        'fastapi==0.103.2',
-        'qrcode==7.4.2',
-        'Pillow==10.0.1',
-        'uvicorn[standard]==0.23.2',
-        'gunicorn==21.2.0',
-        'PyYAML==6.0.1',
-        'python-dotenv==1.0.0',
-        'smart-open==6.4.0',
-        'deepdiff==6.6.0'
+        'cryptography==41.0.4'
     ],
     entry_points={
         # If needed, you can add entry points for command line utilities here
