d298edd208da309ca556a715136e2c2210a98e0b
Venkata Mutyala <venkata@venkatamutyala.com>
2023-10-09 00:30:24 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='glueops-helpers',
-    version='0.0.8',
+    version='0.0.10',
     packages=find_packages(),
     install_requires=[
         'requests',
