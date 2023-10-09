294d7754f070d59f921493faacce02deaf4a26c7
Venkata Mutyala <venkata@venkatamutyala.com>
2023-10-09 00:28:06 -0700

    Create checksum_tools.py

---
+++ glueops/checksum_tools.py
@@ -0,0 +1,14 @@
+import zlib
+
+def string_to_crc32(input_string):
+    """Compute CRC32 checksum for a given string and return it in hexadecimal format."""
+    # Encode the string to bytes
+    data_bytes = input_string.encode('utf-8')
+    
+    # Compute CRC32 checksum
+    checksum = zlib.crc32(data_bytes) & 0xffffffff
+    
+    # Convert checksum to hexadecimal
+    hex_checksum = hex(checksum)
+    
+    return hex_checksum
