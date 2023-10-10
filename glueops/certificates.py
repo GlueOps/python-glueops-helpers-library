cc526527bac3f66a7e45bb9d15015e850a98d0f5
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2023-10-10 20:14:27 +0000

    refactor(certificates): remove exception handling to allow full strack to be passed to implementing app

--- glueops/certificates.py
+++ glueops/certificates.py
@@ -4,20 +4,15 @@ from cryptography.exceptions import UnsupportedAlgorithm, InvalidSignature
 
 
 def extract_serial_number_from_cert_string(cert_string):
-    try:
-        certificate = x509.load_pem_x509_certificate(cert_string.encode(), default_backend())
-        decimal_serial = certificate.serial_number
-        
-        # Convert to hexadecimal
-        hex_serial = format(decimal_serial, 'X')
-        
-        # Ensure an even number of digits for correct byte representation
-        if len(hex_serial) % 2 != 0:
-            hex_serial = '0' + hex_serial
-        
-        # Return serial with colons between every 2 characters
-        return ":".join(hex_serial[i:i+2] for i in range(0, len(hex_serial), 2))
+    certificate = x509.load_pem_x509_certificate(cert_string.encode(), default_backend())
+    decimal_serial = certificate.serial_number
 
-    except (ValueError, UnsupportedAlgorithm, InvalidSignature) as e:
-        # You can specify more exceptions as needed based on the specific errors you're encountering.
-        raise ValueError("Provided certificate string is not parsable: " + str(e))
+    # Convert to hexadecimal
+    hex_serial = format(decimal_serial, 'X')
+
+    # Ensure an even number of digits for correct byte representation
+    if len(hex_serial) % 2 != 0:
+        hex_serial = '0' + hex_serial
+
+    # Return serial with colons between every 2 characters
+    return ":".join(hex_serial[i:i+2] for i in range(0, len(hex_serial), 2))
