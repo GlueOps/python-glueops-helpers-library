1c73eb7d7b1405605f5f2b2955d771fe7f9a24cb
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2023-10-10 02:46:45 +0000

    fix: remove logging config configuring logging multiple times within a project will incease the handlers and cause duplicate logs to be emmitted if the logging module is also used where this function is imported.  if we want to log out that info update, just importing logging and emitting to a project using the glueops log library will work

--- glueops/aws.py
+++ glueops/aws.py
@@ -1,7 +1,4 @@
 import boto3
-import glueops.logging
-
-logger = glueops.logging.configure()
 
 
 def create_aws_client(service):
@@ -10,8 +7,6 @@ def create_aws_client(service):
 
 def get_resource_arns_using_tags(tags, aws_resource_filters):
     """Retrieve ARNs of resources with specific tags."""
-    logger.info(
-        f"Checking to see if this arn was already requested/created with tags: {tags}")
     tagging = create_aws_client('resourcegroupstaggingapi')
     tags = {item['Key']: item['Value'] for item in tags}
     response = tagging.get_resources(
