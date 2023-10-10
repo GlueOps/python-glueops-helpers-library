28f58b2c69d6c8daf8cca3f75fde3cb6919478a9
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2023-10-10 20:31:56 +0000

    docs(aws): add docstrings and typehints

--- glueops/aws.py
+++ glueops/aws.py
@@ -1,12 +1,26 @@
+from typing import List
+
 import boto3
+from botocore.client import BaseClient
 
 
-def create_aws_client(service):
-    return boto3.client(service, region_name='us-east-1')
+def create_aws_client(service: str, region: str = 'us-east-1') -> BaseClient:
+    return boto3.client(service, region_name=region)
 
 
-def get_resource_arns_using_tags(tags, aws_resource_filters):
-    """Retrieve ARNs of resources with specific tags."""
+def get_resource_arns_using_tags(
+    tags: List[dict],
+    aws_resource_filters: List[str]
+) -> List[str]:
+    """Retrieve ARNs of resources with specific tags.
+
+    Args:
+        tags (List[dict]): _in format {"Key": "<the-key>", "Value": "<the-value>"}
+        aws_resource_filters (List[str]): AWS resource filter list
+
+    Returns:
+        List[str]: ARNs of resources
+    """
     tagging = create_aws_client('resourcegroupstaggingapi')
     tags = {item['Key']: item['Value'] for item in tags}
     response = tagging.get_resources(
