095a86fccd2ac556a76fa1a86819c570f7bf9174
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2023-10-10 20:53:49 +0000

    docs(logging): docstrings and typehints

--- glueops/setup_logging.py
+++ glueops/setup_logging.py
@@ -1,6 +1,7 @@
 import inspect
 import json
 import logging
+from typing import Union
 
 
 class JsonFormatter(logging.Formatter):
@@ -18,7 +19,22 @@ class JsonFormatter(logging.Formatter):
         return json.dumps(log_entry)
 
 
-def configure(name=None, level=logging.INFO, *handlers):
+def configure(
+    name=None,
+    level: Union[str, int]=logging.ERROR,
+    *handlers: logging.Handler
+) -> logging.Logger:
+    """Configure and return a logger with GlueOps default configuration
+
+    Args:
+        name (_type_, optional): The name of the logger. Defaults to None.
+        level (Union[str, int], optional): The log level, as an int or str. Defaults to logging.ERROR.
+            Must be less restrictive than the level applied to additional handlers for those handlers to receive logs
+        *handlers (logging.Handler, optional): Add any additional handlers that may be desired.
+
+    Returns:
+        logging.Logger: Instance of configured logger
+    """
     if name is None:
         frame = inspect.stack()[1]
         module = inspect.getmodule(frame[0])
