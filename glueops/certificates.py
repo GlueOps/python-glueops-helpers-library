38fd9c06d80cd74716309125e3150633fc155a6e
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2023-10-10 20:22:29 +0000

    feat(certificates): convert output to uppercase, add typehints

--- glueops/certificates.py
+++ glueops/certificates.py
@@ -3,7 +3,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.exceptions import UnsupportedAlgorithm, InvalidSignature
 
 
-def extract_serial_number_from_cert_string(cert_string):
+def extract_serial_number_from_cert_string(cert_string: str) -> str:
     certificate = x509.load_pem_x509_certificate(cert_string.encode(), default_backend())
     decimal_serial = certificate.serial_number
 
@@ -15,4 +15,4 @@ def extract_serial_number_from_cert_string(cert_string):
         hex_serial = '0' + hex_serial
 
     # Return serial with colons between every 2 characters
-    return ":".join(hex_serial[i:i+2] for i in range(0, len(hex_serial), 2))
+    return ":".join(hex_serial[i:i+2] for i in range(0, len(hex_serial), 2)).upper()
