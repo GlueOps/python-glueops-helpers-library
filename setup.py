b3a67d43949702ec43e1d39e6feff5db41af9c77
Venkata Mutyala <venkata@venkatamutyala.com>
2023-10-15 23:37:17 -0700

    chore: bump for release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='glueops-helpers',
-    version='0.0.11',
+    version='0.2.0',
     packages=find_packages(),
     install_requires=[
         'requests',
