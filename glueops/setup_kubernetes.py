cec399d3b78d748e2396765eb024073bd3edb3ee
Venkata Mutyala <venkata@venkatamutyala.com>
2023-11-08 21:58:02 -0800

    Feat/addding kubernetes api client setup (#4)
    * feat: removing slowapi and adding setup_kubernetes

---
+++ glueops/setup_kubernetes.py
@@ -0,0 +1,24 @@
+import os
+import kubernetes
+
+def load_kubernetes_config(logger):
+    try:
+        if os.getenv('KUBERNETES_SERVICE_HOST'):
+            logger.info("KUBERNETES_SERVICE_HOST has a value so we must be running inside of kubernetes")
+            logger.info('Attempting to load in-cluster Kubernetes configuration')
+            kubernetes.config.load_incluster_config()
+        else:
+            logger.info('Using local Kubeconfig because we are not running in kubernetes')
+            kubernetes.config.load_kube_config()
+        logger.info('Successfully loaded Kubernetes configuration')
+    except kubernetes.config.ConfigException as e:
+        logger.error(f'Kubernetes configuration error: {e}')
+        os.sys.exit('Failed to load Kubernetes configuration, exiting application')
+
+    try:
+        v1 = kubernetes.client.CoreV1Api()
+        custom_api = kubernetes.client.CustomObjectsApi()
+        return v1, custom_api
+    except Exception as e:
+        logger.error(f'Error setting up Kubernetes API clients: {e}')
+        os.sys.exit('Failed to setup Kubernetes API clients, exiting application')
--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='glueops-helpers',
-    version='0.2.0',
+    version='0.3.0',
     packages=find_packages(),
     install_requires=[
         'requests',
         'boto3',
-        'slowapi',
-        'cryptography'
+        'cryptography',
+        'kubernetes'
     ],
     entry_points={
         # If needed, you can add entry points for command line utilities here
