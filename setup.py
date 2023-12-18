759ac9317c8bcb7aabf50ef15fb3b67473efd75a
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2023-12-17 19:58:15 -0700

    chore: update setup to current version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='glueops-helpers',
-    version='0.3.0',
+    version='0.4.1',
     packages=find_packages(),
     install_requires=[
         'requests',
